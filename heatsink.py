9c670eeb759ef02702c91d000be10ec703bd352d
rdylanward <r.dylan.ward@gmail.com>
2021-06-16 19:19:38 +0000

    Added a header and navbars to the project

--- heatsink.py
+++ heatsink.py
@@ -3,6 +3,7 @@ from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -19,8 +20,7 @@ mongodb = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_home")
-def get_home():
+def index():
     return render_template("index.html")
 
 
