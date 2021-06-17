3e27b4301d07fa3141c4a371f2b2c6a8f6b40e9e
rdylanward <r.dylan.ward@gmail.com>
2021-06-17 17:50:34 +0000

    Materialize error resolved. HTML back on track

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+/**********************************************************************/
+/***                     MOBILE NAVBAR SIDENAV                      ***/
+/**********************************-***********************************/
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav({draggable: true});
+});window.onscroll = function() {stickyNavbar()};
