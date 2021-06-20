a68bf47e9e138071acb3a28b3253562b7a09dd5f
rdylanward <r.dylan.ward@gmail.com>
2021-06-20 01:14:14 +0000

    Updated udateMember() code

--- heatsink.py
+++ heatsink.py
@@ -70,8 +70,33 @@ def settings(member, admin):
 
 @app.route("/settings", methods=["GET", "POST"])
 def updateMember(member, admin):
-    member = session["member"]
-    admin = session["admin"]
+    if request.method == "POST":
+        # Initialise parameters
+        member = session["member"]
+        admin = session["admin"]
+        is_admin = "admin" if request.form.get("is_admin") else "user"
+
+        # Set the criteria
+        which_member = {"username": request.form.get("username")}
+        update_to = { 
+            "$set": {
+                "password": generate_password_hash(
+                    request.form.get("password")),
+                "type": is_admin
+            }
+        }
+
+        # Update the document
+        mongodb.db.members.update_one(which_member, update_to)
+
+        # Check to see if it worked
+        specified_user = mongodb.db.members.find_one(
+            {"username": request.form.get("username").lower()})
+        if check_password_hash(specified_user["password"], request.form.get(
+                "password")) and specified_user["type"] == is_admin:
+            flash("Member update successful!")
+            return redirect(url_for('settings', member=member, type=type))
+
     return render_template("settings.html", member=member, admin=admin)
 
 
