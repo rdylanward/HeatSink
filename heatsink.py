3c98538acf8be88b7f3e9125ec1c035acd43d0a6
rdylanward <r.dylan.ward@gmail.com>
2021-06-26 11:41:24 +0000

    Added scrollspy element

--- heatsink.py
+++ heatsink.py
@@ -84,10 +84,7 @@ def settings():
         "settings.html",
         controllers=controllers,
         heaters=heaters,
-        members=members,
-        delete_controllers=delete_controllers,
-        delete_heaters=delete_heaters,
-        delete_members=delete_members)
+        members=members)
 
 
 @app.route("/actionMember/", methods=["GET", "POST"])
@@ -151,6 +148,42 @@ def actionGroup():
 
 @app.route("/actionItems/", methods=["GET", "POST"])
 def actionItems():
+
+    # Initialise parameters
+    is_username = request.form.get("user")
+    is_heater = request.form.get("heater")
+    is_controller = request.form.get("controller")
+
+    if is_username:
+
+        # Find the member
+        member_details = mongodb.db.members.find_one(is_username)
+
+        # Remove the member
+        mongodb.db.members.delete_one({"_id": ObjectId(member_details._id)})
+
+    elif is_heater:
+
+        # Find heater
+        heater_details = mongodb.db.heaters.find_one(is_heater)
+
+        # Remove heater
+        mongodb.db.heaters.delete_one({"_id": ObjectId(heater_details._id)})
+
+        # Remove associated member group
+        for collection_name in mongodb.db.list_collection_names():
+            if (heater_details.name + "_member") in collection_name:
+                collection_to_drop = mongodb.db[collection_name]
+                collection_to_drop.drop()
+
+    elif is_controller:
+
+        # Find controller
+        controller_details = mongodb.db.controllers.find_one(is_controller)
+
+        # Remove controller
+        mongodb.db.heaters.delete_one({"_id": ObjectId(controller_details._id)})
+
     return render_template("settings.html")
 
 
